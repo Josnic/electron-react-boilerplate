be2dbbe0c9be828bd09a6f6f41cbdbffaab4d97e
José N. Herrera <josnikh@hotmail.com>
2023-04-04 19:05:25 -0500

    Locales for apexcharts

---
+++ src/renderer/utils/apexChartLocales.tsx
@@ -0,0 +1,20 @@
+export const SpanishLocale = {
+    name: "es",
+    options: {
+    months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+    shortMonths: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+    days: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+    shortDays: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+    toolbar: {
+          exportToSVG: "Descargar SVG",
+          exportToPNG: "Descargar PNG",
+          menu: "Menu",
+          selection: "Selection",
+          selectionZoom: "Selection Zoom",
+          zoomIn: "Zoom In",
+          zoomOut: "Zoom Out",
+          pan: "Panning",
+          reset: "Reset Zoom"
+      }
+    }
+  }
