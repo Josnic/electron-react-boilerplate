5468e2ae74affcb9c25dec8a4d1147b5951ec759
José N. Herrera <josnikh@hotmail.com>
2023-04-05 16:52:40 -0500

    implements provider redux and implements route module

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,17 +1,13 @@
-import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
-import MyTheme from './MyTheme';
+import { Provider } from 'react-redux';
+import AuthStore from './redux/stores';
+import RouteApp from './route';
 import './styles/globals.scss';
-import Home from './pages/Home';
+
 
 export default function App() {
   return (
-    <ThemeProvider theme={MyTheme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <Provider store={AuthStore}>
+      <RouteApp />
+    </Provider>
   );
 }
