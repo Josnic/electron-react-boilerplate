a902f6b7f1994605544269c875b1d520158d79d8
José N. Herrera <josnikh@hotmail.com>
2023-05-16 11:45:14 -0500

    Self appraisal test fixed

--- src/renderer/components/Tests/SelfAppraisal/index.tsx
+++ src/renderer/components/Tests/SelfAppraisal/index.tsx
@@ -13,101 +13,52 @@ import IconButton from '@mui/material/IconButton';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+
+import RadioQuestion from '../components/RadioQuestion';
 import TestTitle from '../TestTitle';
 import '../styles.scss';
 
-
 const SelfAppraisal = ({ data }) => {
-    
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log((event.target as HTMLInputElement).value);
-      };
+  const [answers, setAnswers] = useState(Array(10).fill(null));
+  const [questions, setQuestions] = useState(Array(10).fill('Pregunta'));
 
-    const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
-        <Tooltip {...props} arrow classes={{ popper: className }}  placement="top" />
-        ))(({ theme }) => ({
-        [`& .${tooltipClasses.tooltip}`]: {
-            backgroundColor: theme.palette.common.white,
-            color: 'rgba(0, 0, 0, 0.87)',
-            boxShadow: theme.shadows[1],
-            fontSize: 11,
-        },
-        [`& .${tooltipClasses.arrow}`]: {
-            color: theme.palette.common.white,
-            "&:before": {
-                border: "1px solid #E6E8ED"
-              },
-        },
-    }));
-    return (
-        <Grid container columns={{ xs: 4, md: 12 }} spacing={2}>
-            <Grid item xs={12}>
-                <TestTitle title={"Autoevaluación"} />
-            </Grid>
-            <Grid item xs={12}>
-               <div className='tests-container'>
-                    <div className='question-container'>
+  const handleAnswerChange = (index, value) => {
+    console.log(index, value);
+    const newAnswers = [...answers];
+    newAnswers[index] = value;
+    setAnswers(newAnswers);
+  };
 
-                        <div className='question self-appraisal'>
-                            Estás desarrollando tu potencial en lo que haces a diario? 
-                            Estás desarrollando tu potencial en lo que haces a diario? 
-                            Estás desarrollando tu potencial en lo que haces a diario?
-                            <LightTooltip title="Add">
-                                <IconButton>
-                                    <LiveHelpIcon />
-                                </IconButton>
-                            </LightTooltip>
-                        </div>
-                        <div className='radio-container'>
-                            <FormControl>
-                                <RadioGroup
-                                    row
-                                    name="row-radio-buttons-group"
-                                    onChange={handleChange}
-                                >
-                                    <FormControlLabel value="1" control={<Radio />} label="1" />
-                                    <FormControlLabel value="2" control={<Radio />} label="2" />
-                                    <FormControlLabel value="3" control={<Radio />} label="3" />
-                                    <FormControlLabel value="4" control={<Radio />} label="4" />
-                                    <FormControlLabel value="5" control={<Radio />} label="5" />
-                                </RadioGroup>
-                            </FormControl>
-                        </div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const total = answers.reduce((a, b) => a + b);
+    alert(`Tu calificación total es: ${total}`);
+  };
 
-                    </div>
-                    <div className='question-container'>
-
-                        <div className='question self-appraisal'>
-                            Estás desarrollando tu potencial en lo que haces a diario? 
-                            Estás desarrollando tu potencial en lo que haces a diario? 
-                            Estás desarrollando tu potencial en lo que haces a diario?
-                        </div>
-                        <div className='radio-container'>
-                            <FormControl>
-                                <RadioGroup
-                                    row
-                                    name="row-radio-buttons-group"
-                                    onChange={handleChange}
-                                >
-                                    <FormControlLabel value="1" control={<Radio />} label="1" />
-                                    <FormControlLabel value="2" control={<Radio />} label="2" />
-                                    <FormControlLabel value="3" control={<Radio />} label="3" />
-                                    <FormControlLabel value="4" control={<Radio />} label="4" />
-                                    <FormControlLabel value="5" control={<Radio />} label="5" />
-                                </RadioGroup>
-                            </FormControl>
-                        </div>
-
-                    </div>
-               </div>
-            </Grid>
-            <Grid item xs={12} className='lessons-button-container-center'>
-                <ButtomCustom variant="contained" rounded>
-                    Continuar
-                </ButtomCustom>
-            </Grid>
-        </Grid>
-    )
-}
+  return (
+    <Grid container columns={{ xs: 4, md: 12 }} spacing={2}>
+      <Grid item xs={12}>
+        <TestTitle title={'Autoevaluación'} color={'white'} />
+      </Grid>
+      <Grid item xs={12}>
+        <div className="tests-container">
+          {questions.map((question, index) => {
+            return (
+              <RadioQuestion
+                key={index}
+                onAnswerChange={(value) => handleAnswerChange(index, value)}
+              />
+            );
+          })}
+        </div>
+      </Grid>
+      <Grid item xs={12} className="lessons-button-container-center">
+        <ButtomCustom onClick={handleSubmit} variant="contained" rounded>
+          Continuar
+        </ButtomCustom>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default SelfAppraisal;
