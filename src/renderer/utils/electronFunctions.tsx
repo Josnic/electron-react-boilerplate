dbccba3fdc8c93958abaef33e98b5b589b515a15
José N. Herrera <josnikh@hotmail.com>
2023-06-05 15:28:20 -0500

    Renderer function for get path resource

--- src/renderer/utils/electronFunctions.tsx
+++ src/renderer/utils/electronFunctions.tsx
@@ -1,3 +1,7 @@
 export const openSystemBrowser = (url) => {
     window.electron.ipcRenderer.sendMessage('openBrowser', [url]);
 }
+
+export const getPathCourseResource = async(path) => {
+    return await window.electron.ipcRenderer.invoke('getPathCourseResource', path);
+}
