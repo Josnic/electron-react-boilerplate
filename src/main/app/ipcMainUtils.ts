a6c712fcaeac91e5ea44e6f0313678513ac2455b
José N. Herrera <josnikh@hotmail.com>
2023-06-05 15:27:35 -0500

    Get path course resource

--- src/main/app/ipcMainUtils.ts
+++ src/main/app/ipcMainUtils.ts
@@ -1,5 +1,15 @@
-export function ipcMainUtils(ipcMain, shell) {
+import { join } from 'path';
+import { app, shell } from 'electron';
+
+export function ipcMainUtils(ipcMain) {
     ipcMain.on('openBrowser', async (event, arg) => {
         shell.openExternal(arg[0]);
+    });
+
+    ipcMain.handle('getPathCourseResource', async(event, ...args) => {
+        return new Promise((resolve) => {
+            const finalPath = ((process.env.NODE_ENV === 'development') ? "file://" : "") + join(app.getAppPath(), "release/app/" + args[0]);
+            resolve(finalPath);
+        });
     });
 }
