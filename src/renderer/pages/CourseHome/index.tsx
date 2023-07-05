284e91a07ceadb614e22967b03416930152714a5
José N. Herrera <josnikh@hotmail.com>
2023-07-05 17:10:47 -0500

    Implements question form component and test components

--- src/renderer/pages/CourseHome/index.tsx
+++ src/renderer/pages/CourseHome/index.tsx
@@ -13,18 +13,27 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import ListItem from '@mui/material/ListItem';
+import Grid from '@mui/material/Grid';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import HomeIcon from '@mui/icons-material/Home';
 import MenuHeader from '../../components/Layout/MenuHeader';
 import MenuFooter from '../../components/Layout/MenuFooter';
 //https://github.com/mui/material-ui/blob/v5.11.16/docs/data/material/getting-started/templates/dashboard/Dashboard.tsx
 const drawerWidth = 300;
+import ListCard from '../../components/ListCard';
 import MenuTreeView from './components/MenuTreeView';
+import ContentRenderer from 'renderer/components/ContentRenderer';
+import FormQuestion from '../../components/FormQuestion';
+import RadioButtonTest from '../../components/Tests/RadioButtonTest';
+import InputNumberTest from '../../components/Tests/InputNumberTest'
 
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { sqlite3All } from '../../helpers/Sqlite3Operations'
+
+import "./styles.scss";
 interface Props {
   window?: () => Window;
 }
@@ -81,14 +90,74 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 export default function CourseHome() {
   const theme = useTheme();
   const [open, setOpen] = React.useState(true);
-
+  const [showUnits, setShowUnits] = React.useState(true);
+  const [units, setUnits] = React.useState([]);
+  const [dataMenu, setDataMenu] = React.useState([]);
+  const [htmlContent, setHtmlContent] = React.useState(null);
+  const [formData, setFormData] = React.useState(null);
+  const [testData, setTestData] = React.useState(null);
+  const menuTreeViewRef = React.useRef();
+  const navigate = useNavigate();
   const handleDrawerOpen = () => {
     setOpen(true);
   };
+  const [nextNodeId, setNextNodeId] = React.useState(null);
+  const { courseCode } = useParams();
 
   const handleDrawerClose = () => {
     setOpen(false);
   };
+
+  const loadMenu = async() => {
+    const units = await sqlite3All(`SELECT *, imagen_unid AS imagen, video AS videos FROM unidades WHERE cod_curso = '${courseCode}' ORDER BY orden ASC`);
+    let dataMenu = [];
+
+    if (units.OK){
+      dataMenu = units.OK;
+      setUnits(units.OK);
+      for (let i = 0; i < dataMenu.length; i++) {
+        const lessons = await sqlite3All(`SELECT *, objetivo AS contenido  FROM lecciones WHERE cod_unidad = '${dataMenu[i].cod_unidad}' ORDER BY cod_leccion ASC`);
+        if (lessons.OK){
+          for (let j = 0; j < lessons.OK.length; j++) {
+            const sublessons = await sqlite3All(`SELECT *, CASE WHEN test_id IS NULL THEN '' ELSE (SELECT test_tipo FROM tests WHERE cod_test = test_id) END AS test_tipo 
+                                                 FROM sublecciones WHERE cod_leccion = '${lessons.OK[j].cod_leccion}' ORDER BY orden ASC`);
+            if (sublessons.OK){
+              lessons.OK[j]["sublessons"] = sublessons.OK;
+            }
+          }
+        }
+        dataMenu[i]["lessons"] = lessons.OK;
+      }
+    }
+    setDataMenu(dataMenu);
+    console.log(dataMenu)
+  }
+
+  const renderContent = async(type, data) => {
+    console.log(type, data)
+    setShowUnits(false);
+
+    if (data.cod_formulario && data.cod_formulario != ""){
+      setHtmlContent(null);
+      setTestData(null);
+      setFormData(data);
+    }else if (data.test_id && data.test_id != ""){
+      setFormData(null);
+      setHtmlContent(null);
+      setTestData(data);
+    }else{
+      setFormData(null);
+      setTestData(null);
+      setHtmlContent({
+        type: type,
+        data: data
+      })
+    }
+  }
+
+  React.useEffect(()=>{
+    loadMenu();
+  }, []);
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -117,7 +186,9 @@ export default function CourseHome() {
         open={open}
       >
         <DrawerHeader>
-          <IconButton style={{ marginRight: "50%" }} onClick={handleDrawerClose}>
+          <IconButton style={{ marginRight: "50%" }} onClick={()=>{
+            navigate("/home");
+          }}>
             <HomeIcon /> Inicio
           </IconButton>
           <IconButton onClick={handleDrawerClose}>
@@ -129,41 +200,80 @@ export default function CourseHome() {
           </IconButton>
         </DrawerHeader>
         <Divider />
-        <MenuTreeView />
+        <MenuTreeView 
+          data={dataMenu} 
+          onClickItem={(type, data, nextNodeIdData) => {
+            renderContent(type, data)
+          }}
+          ref={menuTreeViewRef} 
+        />
       </Drawer>
       <Main open={open} className="f-container fixed-hf">
         <DrawerHeader />
         <Box className="main">
-          <Typography paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Rhoncus
-            dolor purus non enim praesent elementum facilisis leo vel. Risus at
-            ultrices mi tempus imperdiet. Semper risus in hendrerit gravida
-            rutrum quisque non tellus. Convallis convallis tellus id interdum
-            velit laoreet id donec ultrices. Odio morbi quis commodo odio aenean
-            sed adipiscing. Amet nisl suscipit adipiscing bibendum est ultricies
-            integer quis. Cursus euismod quis viverra nibh cras. Metus vulputate
-            eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo
-            quis imperdiet massa tincidunt. Cras tincidunt lobortis feugiat
-            vivamus at augue. At augue eget arcu dictum varius duis at
-            consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-            donec massa sapien faucibus et molestie ac.
-          </Typography>
-          <Typography paragraph>
-            Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-            ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar
-            elementum integer enim neque volutpat ac tincidunt. Ornare
-            suspendisse sed nisi lacus sed viverra tellus. Purus sit amet
-            volutpat consequat mauris. Elementum eu facilisis sed odio morbi.
-            Euismod lacinia at quis risus sed vulputate odio. Morbi tincidunt
-            ornare massa eget egestas purus viverra accumsan in. In hendrerit
-            gravida rutrum quisque non tellus orci ac. Pellentesque nec nam
-            aliquam sem et tortor. Habitant morbi tristique senectus et.
-            Adipiscing elit duis tristique sollicitudin nibh sit. Ornare aenean
-            euismod elementum nisi quis eleifend. Commodo viverra maecenas
-            accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam
-            ultrices sagittis orci a.
-          </Typography>
+          {
+            showUnits ? (
+              <div className='list-units-container'>
+                <Grid container columns={{ xs: 4, md: 12 }} spacing={2}>
+                  {units.map((card, index) => (
+                    <Grid item xs={4} key={index}>
+                      <ListCard cardData={card} onCardClick={()=>{
+                        //navigate("/course/" + card.cod_curso);
+                        menuTreeViewRef.current.setSelectedNode(`UNIT-${index}-${card.cod_unidad}`)
+                      }}/>
+                    </Grid>
+                  ))}
+                </Grid>
+            </div>
+            ):(
+              <>
+              {
+                htmlContent ? (
+                  <ContentRenderer 
+                    data={htmlContent.data} 
+                    type={htmlContent.type} 
+                    courseCode={courseCode}
+                    onContinue={()=> {
+                      
+                    }} 
+                  />
+                ):(
+                  null
+                )
+              }
+              {
+                formData ? (
+                  <FormQuestion 
+                    data={formData} 
+                    courseCode={courseCode}
+                  />
+                ):(
+                  null
+                )
+              }
+              {
+                testData && testData.test_tipo == "RADIO" ? (
+                  <RadioButtonTest 
+                    data={testData} 
+                    courseCode={courseCode}
+                  />
+                ):(
+                  null
+                )
+              }
+              {
+                testData && testData.test_tipo == "INPUTN" ? (
+                  <InputNumberTest 
+                    data={testData} 
+                    courseCode={courseCode}
+                  />
+                ):(
+                  null
+                )
+              }
+              </>
+            )
+          }
         </Box>
 
         <Box className="footer">
