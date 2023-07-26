c887a12fdff9149200d4c488a856ec52c23adea8
José N. Herrera <josnikh@hotmail.com>
2023-07-26 11:43:20 -0500

    Implements register user form

--- src/renderer/pages/Register/index.tsx
+++ src/renderer/pages/Register/index.tsx
@@ -10,6 +10,9 @@ import Grid from '@mui/material/Grid';
 import Chip from '@mui/material/Chip';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import IconButton from '@mui/material/IconButton';
+import ArrowCircleRightTwoTone from '@mui/icons-material/ArrowCircleRightTwoTone';
+import ArrowCircleLeftTwoTone from '@mui/icons-material/ArrowCircleLeftTwoTone';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -17,155 +20,337 @@ import { ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import Copyright from '../../components/Copyright';
 import OverlayLoader from '../../components/OverlayLoader';
-
+import DatePicker, { registerLocale, setDefaultLocale } from 'react-datepicker';
+import es from 'date-fns/locale/es';
+registerLocale('es', es);
 import { showToast } from '../../utils/toast';
-import { validateEmail } from '../../utils/generals';
-import { sqlite3Run } from '../../helpers/Sqlite3Operations'; 
+import {
+  validateEmail,
+  getMysqlDate,
+  base64Encode,
+  base64Decode,
+  sha256Encode,
+  formatDate
+} from '../../utils/generals';
+import { sqlite3Run } from '../../helpers/Sqlite3Operations';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import './styles.scss';
 
 export default function SignUp() {
+  const range = (start, end) => {
+    var ans = [];
+    for (let i = start; i <= end; i++) {
+      ans.push(i);
+    }
+    return ans;
+  };
+  const getYear = (date) => {
+    return date.getYear();
+  };
+  const years = range(1990, new Date().getFullYear() + 1, 1);
+  const months = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Novimbre',
+    'Diciembre',
+  ];
+
   const [open, setOpen] = React.useState(false);
   const [ready, setReady] = React.useState(false);
   const navigate = useNavigate();
+  const [startDate, setStartDate] = React.useState(new Date());
 
-  const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const handleChange = (e) => {
+    setIsOpen(false);
+    setStartDate(e);
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setIsOpen(true);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
 
     const email = data.get('email').trim();
-    const firstName = data.get('firstName').trim();
-    const lastName = data.get('lastName').trim();
+    const email_repeat = data.get('email_repeat').trim();
+    const fullName = data.get('fullName').trim();
+    const document = data.get('document').trim();
     const password = data.get('password').trim();
-    if (email == "" || password == "" || firstName == "" || lastName == "") {
-      showToast("Completa los datos");
-    }else if (!validateEmail(email)) {
-      showToast("El correo electrónico no es correcto");
-    }else{
+    const password_repeat = data.get('password_repeat').trim();
+    const birthday = startDate ? formatDate(startDate) : '';
+    if (
+      email == '' ||
+      password == '' ||
+      fullName == '' ||
+      document == '' ||
+      birthday == ""
+    ) {
+      showToast('Completa los datos');
+    } else if (!validateEmail(email)) {
+      showToast('El correo electrónico no es correcto');
+    } else {
+      alert(sha256Encode(password))
       setOpen(true);
-      const result = await sqlite3Run("INSERT INTO user VALUES (?,?,?,?)", [firstName, lastName, email, password]);
-      setReady(true);
-      showToast("Usuario creado exitosamente", "success", undefined, ()=>{
-        setOpen(false);
-        navigate("/login");
-      })
+      const result = await sqlite3Run(
+        'INSERT INTO usuarios VALUES (?,?,?,?,?,?,?)',
+        [
+          fullName,
+          document,
+          email,
+          sha256Encode(password),
+          base64Encode(password),
+          birthday,
+          getMysqlDate().split(' ')[0],
+        ]
+      );
       if (result.OK) {
         setReady(true);
-        showToast("Usuario creado exitosamente. De click en el botón o cierre el mensaje para ir al ", "success", undefined, ()=>{
-          setOpen(false);
-          navigate("/login");
-        })
-      }else{
-        //setOpen(false);
-        console.log(result)
-        //showToast("Ocurrió un error. Intenta nuevamente.", "error");
+        showToast(
+          'Usuario creado exitosamente. De click en el botón o cierre el mensaje para ir al ',
+          'success',
+          undefined,
+          () => {
+            setOpen(false);
+            navigate('/login');
+          }
+        );
+      } else {
+        setOpen(false);
+        console.log(result);
+        showToast('Ocurrió un error. Intenta nuevamente.', 'error');
       }
     }
   };
 
   const CloseButton = ({ closeToast }) => (
-    <Chip label="Iniciar sesión" variant="outlined" onClick={()=>{
-      closeToast();
-      setOpen(false);
-      navigate("/login");
-    }} />
+    <Chip
+      label="Iniciar sesión"
+      variant="outlined"
+      onClick={() => {
+        closeToast();
+        setOpen(false);
+        navigate('/login');
+      }}
+    />
   );
 
   return (
-      <Container component="main" maxWidth="xs">
-        <OverlayLoader open={open} ready={ready} buttonClick={()=>{
+    <Container component="main" maxWidth="xs">
+      <OverlayLoader
+        open={open}
+        ready={ready}
+        buttonClick={() => {
           setOpen(false);
-          navigate("/login");
-        }}/>
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Registro de usuario
-          </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="Nombres"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Apellidos"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Correo electrónico"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Contraseña"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="Acepto los terminos y condiciones."
-                />
-              </Grid>
+          navigate('/login');
+        }}
+      />
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Registro de usuario
+        </Typography>
+        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoComplete="off"
+                name="fullName"
+                required
+                fullWidth
+                id="fullName"
+                label="Nombre completo"
+                autoFocus
+              />
             </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Registrar
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
+            <Grid item xs={12}>
+            <TextField
+                autoComplete="off"
+                name="birthday"
+                required
+                fullWidth
+                id="birthday"
+                label="Fecha de nacimiento"
+                onClick={handleClick}
+                value={formatDate(startDate)}
+                inputProps={
+                  { readOnly: true, }
+                }
+              />
+              {isOpen ? (
+              <DatePicker
+           
+                inline
+                locale="es"
+                dateFormat="yyyy-MM-dd"
+                selected={startDate}
+                onChange={(date) => handleChange(date)}
+                renderCustomHeader={({
+                  date,
+                  changeYear,
+                  changeMonth,
+                  decreaseMonth,
+                  increaseMonth,
+                  prevMonthButtonDisabled,
+                  nextMonthButtonDisabled,
+                }) => (
+                  <div
+                    style={{
+                      margin: 10,
+                      display: 'flex',
+                      justifyContent: 'center',
+                    }}
+                  >
+                    <IconButton
+                      color="secondary"
+                      onClick={decreaseMonth}
+                      disabled={prevMonthButtonDisabled}
+                    >
+                      <ArrowCircleLeftTwoTone />
+                    </IconButton>
+                    <select
+                      value={date.getFullYear()}
+                      onChange={({ target: { value } }) => changeYear(value)}
+                    >
+                      {years.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+
+                    <select
+                      value={months[date.getMonth()]}
+                      onChange={({ target: { value } }) =>
+                        changeMonth(months.indexOf(value))
+                      }
+                    >
+                      {months.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+
+                    <IconButton
+                      color="secondary"
+                      onClick={increaseMonth}
+                      disabled={nextMonthButtonDisabled}
+                    >
+                      <ArrowCircleRightTwoTone />
+                    </IconButton>
+                  </div>
+                )}
+                selected={startDate}
+              />
+              ):(null)}
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="document"
+                label="Documento"
+                name="document"
+                autoComplete="off"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email"
+                label="Correo electrónico"
+                name="email"
+                autoComplete="off"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                id="email_repeat"
+                label="Confirmación correo electrónico"
+                name="email_repeat"
+                autoComplete="off"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password"
+                label="Contraseña"
+                type="password"
+                id="password"
+                autoComplete="new-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                required
+                fullWidth
+                name="password_repeat"
+                label="Contraseña"
+                type="password"
+                id="password_repeat"
+                autoComplete="off"
+              />
+            </Grid>
+            {/*<Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="Acepto los terminos y condiciones."
+              />
+                      </Grid>*/}
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Registrar
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
               <Link
                 component="button"
                 variant="body2"
                 onClick={() => {
-                  navigate("/login")
+                  navigate('/login');
                 }}
               >
                 Iniciar sesión
               </Link>
-              </Grid>
             </Grid>
-          </Box>
+          </Grid>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
-        <ToastContainer limit={3} autoClose={3000} closeButton={CloseButton}/>
-      </Container>
+      </Box>
+      <Copyright sx={{ mt: 5 }} />
+      <ToastContainer limit={3} autoClose={3000} />
+    </Container>
   );
 }
