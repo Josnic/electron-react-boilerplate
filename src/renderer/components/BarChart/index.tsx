966a23e028e994368c1bd5d2703adea123585f6b
José N. Herrera <josnikh@hotmail.com>
2023-07-30 19:29:13 -0500

    Set props in chart options and format code

--- src/renderer/components/BarChart/index.tsx
+++ src/renderer/components/BarChart/index.tsx
@@ -1,109 +1,108 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef,
+  forwardRef,
+  useImperativeHandle,
+} from 'react';
 import ReactApexChart from 'react-apexcharts';
 import { SpanishLocale } from '../../utils/apexChartLocales';
 import './styles.scss';
 
-const BarChart = ({title, series, categories}) =>{
-    const chartInstance = useRef();
+const BarChart = forwardRef(({ title, series, categories }, ref) => {
+  const chartInstance = useRef();
 
-    var colors = [
-        '#FF4560',
-        '#FEB019',
-        '#00E396',
-        '#008FFB',
-      ]
+  var colors = ['#FF4560', '#FEB019', '#00E396', '#008FFB'];
 
-    const [state, setState] = useState({
-          
-        series: [{
-            name:'',
-          data: [21, 22, 10, 28]
-        }],
-        options: {
-          chart: {
-            height: 350,
-            type: 'bar',
-            events: {
-              click: function(chart, w, e) {
-                // console.log(chart, w, e)
-              }
-            },
-            locales: [SpanishLocale],
-            defaultLocale: "es"
+  const state = {
+    series: series,
+    options: {
+      chart: {
+        height: 350,
+        type: 'bar',
+        events: {
+          click: function (chart, w, e) {
+            // console.log(chart, w, e)
           },
-          title: {
-            text: "Su nivel DISC",
-            align: 'center',
-            margin: 10,
-            offsetX: 0,
-            offsetY: 0,
-            floating: false,
-            style: {
-              fontSize:  '20px',
-              fontWeight:  'bold',
-              fontFamily:  undefined,
-              color:  '#263238'
-            },
         },
-          colors: colors,
-          plotOptions: {
-            bar: {
-              columnWidth: '45%',
-              distributed: true,
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          legend: {
-            show: false
-          },
-          xaxis: {
-            categories: [
-              'D',
-              'I',
-              'S',
-              'C'
-            ],
-            labels: {
-              style: {
-                colors: colors,
-                fontSize: '12px'
-              }
-            }
-          }
+        locales: [SpanishLocale],
+        defaultLocale: 'es',
+      },
+      title: {
+        text: title,
+        align: 'center',
+        margin: 10,
+        offsetX: 0,
+        offsetY: 0,
+        floating: false,
+        style: {
+          fontSize: '20px',
+          fontWeight: 'bold',
+          fontFamily: undefined,
+          color: '#263238',
         },
-      
-      
-      })
+      },
+      colors: colors,
+      plotOptions: {
+        bar: {
+          columnWidth: '45%',
+          distributed: true,
+        },
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      legend: {
+        show: true,
+      },
+      xaxis: {
+        categories: categories,
+        labels: {
+          style: {
+            colors: colors,
+            fontSize: '12px',
+          },
+        },
+      },
+    },
+  };
 
-      useEffect(()=>{
-        if (chartInstance.current){
-         //chartInstance.current.chart.setLocale('es')
-         exportToBase64();
-        }
-      }, [chartInstance.current])
+  useEffect(() => {
+    if (chartInstance.current) {
+      chartInstance.current.chart.setLocale('es');
+      //exportToBase64();
+    }
+  }, [chartInstance.current]);
 
-      const exportToBase64 = async () => {
-        await chartInstance.current.chart.dataURI().then(data =>{
-          console.log(data)
-        })
-        const paper = chartInstance.current.chart.w.globals.dom.Paper;
-        const svg = paper.svg()
-        console.log(svg)
-      }
+  const exportToBase64 = async () => {
+    await chartInstance.current.chart.dataURI().then((data) => {
+      console.log(data);
+    });
+    const paper = chartInstance.current.chart.w.globals.dom.Paper;
+    const svg = paper.svg();
+    console.log(svg);
+  };
 
-    return (
-        <>
-            <ReactApexChart
-                ref={chartInstance}
-                options={state.options}
-                series={state.series}
-                type="bar"
-                height={350}
-          />
-        </>
-    )
-}
+  const getBase64Image = async () => {
+    return await hartInstance.current.chart.dataURI();
+  };
+
+  React.useImperativeHandle(ref, () => ({
+    getBase64Image: getBase64Image,
+  }));
+
+  return (
+    <>
+      <ReactApexChart
+        ref={chartInstance}
+        options={state.options}
+        series={state.series}
+        type="bar"
+        width={500}
+        height={400}
+      />
+    </>
+  );
+});
 
 export default BarChart;
