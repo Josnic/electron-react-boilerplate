3f27fc087678f410d7b89a5da53d24ceaa7f1fe6
José N. Herrera <josnikh@hotmail.com>
2023-08-01 13:15:46 -0500

    implements welcome video

--- src/renderer/pages/Welcome/index.tsx
+++ src/renderer/pages/Welcome/index.tsx
@@ -4,57 +4,70 @@ import { useNavigate } from 'react-router-dom';
 
 import ButtomCustom from '../../components/ButtonRound';
 import Artplayer from '../../components/ArtPlayer';
-
+import { getPathCourseResource } from '../../utils/electronFunctions';
 
 export default function Welcome() {
-    const navigate = useNavigate();
-    const goToLogin = () =>{
-        navigate("/login")
-    }
-    return (
-        <Grid container columns={{ xs: 4, md: 12 }} spacing={2}>
-            <Grid item xs={12}>
-                <Artplayer
-                    option={{
-                        url: 'https://artplayer.org/assets/sample/video.mp4',
-                        setting: true,
-                        autoSize: true,
-                        autoMini: true,
-                        screenshot: true,
-                        setting: true,
-                        loop: true,
-                        flip: true,
-                        playbackRate: true,
-                        aspectRatio: true,
-                        fullscreen: true,
-                        fullscreenWeb: true,
-                        miniProgressBar: true,
-                        mutex: true,
-                        backdrop: true,
-                        playsInline: true,
-                        autoPlayback: true,
-                        airplay: true,
-                        theme: '#23ade5',
-                        lang: 'es',
-                        whitelist: ['*'],
-                        moreVideoAttr: {
-                            crossOrigin: 'anonymous',
-                        }
-                    }}
-                    style={{
-                        maxWidth: '800px',
-                        height: '600px',
-                        margin: 'auto'
-                    }}
-                    getInstance={(art) => console.info(art)}
-                />
-            </Grid>
+  const navigate = useNavigate();
+  const goToLogin = () => {
+    navigate('/login');
+  };
+  const [urlVideo, setUrlVideo] = React.useState(null);
 
-            <Grid item xs={12} className='lessons-button-container-center'>
-                <ButtomCustom onClick={goToLogin} variant="contained" rounded>
-                    Continuar
-                </ButtomCustom>
-            </Grid>
-        </Grid>
-    );
+  const getUrlVideo = async() => {
+    const video = await getPathCourseResource('commonassets/bienvenida.mp4');
+    setUrlVideo(video);
+  }
+
+  React.useEffect(()=>{
+    getUrlVideo();
+  }, [])
+  return (
+    <Grid container columns={{ xs: 4, md: 12 }} spacing={2}>
+      <Grid item xs={12}>
+        {urlVideo ? (
+          <Artplayer
+            option={{
+              url: urlVideo,
+              setting: true,
+              autoplay: true,
+              autoSize: true,
+              autoMini: true,
+              screenshot: true,
+              setting: true,
+              loop: true,
+              flip: true,
+              playbackRate: true,
+              aspectRatio: true,
+              fullscreen: true,
+              fullscreenWeb: true,
+              miniProgressBar: true,
+              mutex: true,
+              backdrop: true,
+              playsInline: true,
+              autoPlayback: true,
+              airplay: true,
+              theme: '#23ade5',
+              lang: 'es',
+              whitelist: ['*'],
+              moreVideoAttr: {
+                crossOrigin: 'anonymous',
+              },
+            }}
+            style={{
+              maxWidth: '800px',
+              height: '600px',
+              margin: 'auto',
+            }}
+            getInstance={(art) => console.info(art)}
+          />
+        ) : null}
+      </Grid>
+
+      <Grid item xs={12} className="lessons-button-container-center">
+        <ButtomCustom onClick={goToLogin} variant="contained" rounded>
+          Continuar
+        </ButtomCustom>
+      </Grid>
+    </Grid>
+  );
 }
