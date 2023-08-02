ab451cf36f63d094b170486398c9389c42c45c58
José N. Herrera <josnikh@hotmail.com>
2023-08-02 13:43:46 -0500

    Support to embed Font and styles as alignment, size and fontfamily

--- src/renderer/helpers/ExportPDF.tsx
+++ src/renderer/helpers/ExportPDF.tsx
@@ -1,4 +1,5 @@
-import { PDFDocument } from 'pdf-lib';
+import { PDFDocument, PDFForm, StandardFonts, TextAlignment, setFontAndSize, PDFFont } from 'pdf-lib';
+import fontkit from '@pdf-lib/fontkit';
 import { getPathCourseResource, getBinaryContent } from '../utils/electronFunctions';
 import * as download from 'downloadjs/download';
 
@@ -12,13 +13,38 @@ const base64ToArrayBuffer = (base64String) => {
     return bytes.buffer;
 } 
 
-export default async(objText, arObjImage, pdfDocumentPath) => {
-    const fileBytes = await getBinaryContent(pdfDocumentPath)
+export default async(objText, arObjImage, pdfDocumentPath, additionalFonts) => {
+    const fileBytes = await getBinaryContent(pdfDocumentPath);
     const pdfDoc = await PDFDocument.load(fileBytes);
+    const arFonts = [];
+    if (additionalFonts && Array.isArray(additionalFonts)){
+        pdfDoc.registerFontkit(fontkit);
+        for (let i = 0; i < additionalFonts.length; i++){
+            const fontBinary = await getBinaryContent(additionalFonts[i].font);
+            arFonts.push({
+                name: additionalFonts[i].name,
+                font: await pdfDoc.embedFont(fontBinary)
+            })
+        }
+    }
+    
     const form = pdfDoc.getForm();
     Object.entries(objText).forEach(([key, value]) => {
         let textField = form.getTextField(key);
-        textField.setText(value);
+        textField.setText(value.value);
+        if (value.textAlign){
+            textField.setAlignment(TextAlignment[value.textAlign]);
+        }
+
+        if (value.fontSize){
+            textField.setFontSize(value.fontSize);
+        }
+        if (value.fontFamily){
+            const fontTTF = arFonts.filter(ele => ele.name == value.fontFamily);
+            if (fontTTF.length > 0){
+                textField.updateAppearances(fontTTF[0].font)
+            }
+        }
     });
     for (let i = 0; i < arObjImage.length; i++) {
         let textFieldImage = form.getTextField(arObjImage[i].textField);
@@ -28,7 +54,11 @@ export default async(objText, arObjImage, pdfDocumentPath) => {
         }else{
             embedImage = await pdfDoc.embedJpg(base64ToArrayBuffer(arObjImage[i].image));
         }
+        if (arObjImage[i].textAlign){
+            textFieldImage.setAlignment(TextAlignment[arObjImage[i].textAlign]);
+         }
         textFieldImage.setImage(embedImage);
+        
     }
     const pdfBytes = await pdfDoc.save();
     const filenameParts = pdfDocumentPath.split("/");
