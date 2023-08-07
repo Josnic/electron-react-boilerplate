f3c4a2590855c0359afac46b00c25f043ef24e6a
José N. Herrera <josnikh@hotmail.com>
2023-08-07 12:43:54 -0500

    New nodejs methods

--- src/main/app/ipcMainUtils.ts
+++ src/main/app/ipcMainUtils.ts
@@ -1,23 +1,93 @@
 import { join } from 'path';
 import { app, shell } from 'electron';
 import * as fs from 'fs';
+import { machineIdSync } from 'node-machine-id';
+import { isInternetAvailable } from 'is-internet-available'
 
 export function ipcMainUtils(ipcMain) {
-    ipcMain.on('openBrowser', async (event, arg) => {
-        shell.openExternal(arg[0]);
-    });
+  ipcMain.on('openBrowser', async (event, arg) => {
+    shell.openExternal(arg[0]);
+  });
 
-    ipcMain.handle('getPathCourseResource', async(event, ...args) => {
-        return new Promise((resolve) => {
-            const finalPath = ((process.env.NODE_ENV === 'development') ? "file://" : "") + join(app.getAppPath(), "release/app/" + args[0]);
-            resolve(finalPath);
+  ipcMain.handle('getPathCourseResource', async (event, ...args) => {
+    return new Promise((resolve) => {
+      const finalPath =
+        (process.env.NODE_ENV === 'development' ? 'file://' : '') +
+        join(app.getAppPath(), 'release/app/' + args[0]);
+      resolve(finalPath);
+    });
+  });
+
+  ipcMain.handle('getBinaryContent', async (event, ...args) => {
+    return new Promise((resolve) => {
+      const file = fs.readFileSync(
+        join(app.getAppPath(), 'release/app', args[0])
+      );
+      resolve(file);
+    });
+  });
+
+  ipcMain.handle('isInternetAvailable', async (event, ...args) => {
+    const domain = args[0] ? args[0] : "google.com";
+    return new Promise((resolve) => {
+        isInternetAvailable({ authority: domain }).then((status)=>{
+            resolve(status)
+        }).catch((erro)=>{
+            resolve(false);
         });
     });
+  });
 
-    ipcMain.handle('getBinaryContent', async(event, ...args) => {
-        return new Promise((resolve) => {
-            const file = fs.readFileSync(join(app.getAppPath(), "release/app", args[0]));
-            resolve(file);
-        });
+  ipcMain.handle('getMachineId', async (event, ...args) => {
+    return new Promise((resolve) => {
+      resolve(machineIdSync());
     });
+  });
+
+  ipcMain.handle('deleteSerialFiles', async (event, ...args) => {
+    return new Promise((resolve) => {
+        try{
+            fs.unlinkSync(join(app.getAppPath(), 'release/app', 'fslite.qoc'));
+            resolve({
+                ok:true
+            })
+        }catch(err){
+            resolve({
+                error:err
+            })
+        }
+    });
+  });
+
+  ipcMain.handle('readSerialFiles', async (event, ...args) => {
+    return new Promise((resolve) => {
+      let data = fs.readFileSync(
+        join(app.getAppPath(), 'release/app', 'fslite.qoc'),
+        { encoding: 'utf8' }
+      );
+      try {
+        data = data.split('\n');
+        currentSerial = args[0];
+        let found = false;
+        for (var i = 0; i < data.length; i++) {
+          console.log(data[i] + '---' + currentSerial);
+          if ((data[i]).trim() == currentSerial) {
+            found = true;
+            i = data.length;
+          }
+        }
+        resolve({
+          ok: true,
+          found: found,
+          data: null,
+        });
+      } catch (err) {
+        resolve({
+          error: true,
+          data: err,
+          found: false,
+        });
+      }
+    });
+  });
 }
--- src/renderer/utils/electronFunctions.tsx
+++ src/renderer/utils/electronFunctions.tsx
@@ -9,3 +9,19 @@ export const getPathCourseResource = async(path) => {
 export const getBinaryContent = async(path) => {
     return await window.electron.ipcRenderer.invoke('getBinaryContent', path);
 }
+
+export const getMachineId = async() => {
+    return await window.electron.ipcRenderer.invoke('getMachineId');
+}
+
+export const deleteSerialFiles = async() => {
+    return await window.electron.ipcRenderer.invoke('deleteSerialFiles');
+}
+
+export const readSerialFiles = async(pin) => {
+    return await window.electron.ipcRenderer.invoke('readSerialFiles', pin);
+}
+
+export const isInternetAvailable = async(domain) => {
+    return await window.electron.ipcRenderer.invoke('isInternetAvailable', domain);
+}
